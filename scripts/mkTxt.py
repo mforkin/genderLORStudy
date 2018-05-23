1026118e2eaaf1ca7ac35502c0776536be3804f4
Mike Forkin <mforkin@ccri.com>
2018-05-22 22:25:35 -0400

    Updating mkTxt to use pytesseract
    Other libs don't work on old macs, this seems to work ok.

--- scripts/mkTxt.py
+++ scripts/mkTxt.py
@@ -1,10 +1,9 @@
-from PyPDF2 import PdfFileWriter, PdfFileReader
 import os
 import subprocess
+from PIL import Image
+import pytesseract
 #import sys
 
-#sys.path.append('/Library/Frameworks/Python.framework/Versions/3.7/bin/')
-#import pdf2txt
 
 rootdir = '/Users/mike/resumeData'
 
@@ -12,11 +11,10 @@ for subdir, dirs, files in os.walk(rootdir):
     for file in files:
         if subdir.endswith("pages"):
             filepath = subdir + os.sep + file
-            txtFPath = subdir[0:-5] + "txt" + os.sep + file[0:-4] + ".txt"
+            txtFPath = subdir[0:-5] + "txt" + os.sep + file[0:-5] + ".txt"
 
-            if filepath.endswith(".pdf"):
-                #print(filepath)
-                #print(txtFPath)
-                proc = subprocess.Popen(["bash", "-c", "python3.7 /Library/Frameworks/Python.framework/Versions/3.7/bin/pdf2txt.py '" + filepath + "' >> '" + txtFPath + "'"])
-                #txt = proc.communicate()[0].decode('utf-8')
-                #print(txt)
+            if filepath.endswith(".jpg"):
+                txt = pytesseract.image_to_string(Image.open(filepath))
+                with open(txtFPath, 'w') as wf:
+                    wf.write(txt)
+
