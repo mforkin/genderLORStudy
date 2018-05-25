c01ecc8904b97db00f265d3ba9af2c7824ed7dfc
Mike Forkin <mforkin@ccri.com>
2018-05-24 21:13:04 -0400

    smarter gender sorting

--- scripts/sortByGender.py
+++ scripts/sortByGender.py
@@ -5,9 +5,9 @@ import subprocess
 
 rootdir = '/Users/mike/resumeData'
 
-males = 0
-females = 0
-unsure = 0
+males = set() 
+females = set() 
+unsure = set()
 
 for subdir, dirs, files in os.walk(rootdir):
     for file in files:
@@ -16,22 +16,32 @@ for subdir, dirs, files in os.walk(rootdir):
         if subdir.endswith("txt"):
             filepath = subdir + os.sep + file
             if filepath.endswith(".txt"):
+                name = "_".join(filepath.split("_")[0:2])
                 numHes = 0
                 numShes = 0
-                with open(filepath) as f:
-                    content = f.read()
-                    numHes = len(re.findall(" he ", content))
-                    numShes = len(re.findall(" she ", content))
-                if numHes > numShes:
-                    os.rename(filepath, subdir + os.sep + "male" + os.sep + file)
-                    males = males + 1
-                elif numShes > numHes:
-                    os.rename(filepath, subdir + os.sep + "female" + os.sep + file)
-                    females = females + 1
+                numHis = 0
+                numHer = 0
+                if name in males:
+                    numHes = 1
+                elif name in females:
+                    numShes = 1
                 else:
-                    unsure = unsure + 1
+                    with open(filepath) as f:
+                        content = f.read()
+                        numHes = len(re.findall(" he ", content))
+                        numShes = len(re.findall(" she ", content))
+                        numHis = len(re.findall(" his ", content))
+                        numHer = len(re.findall(" her ", content))
+                if (numHes + numHis) > (numShes + numHer):
+                    os.rename(filepath, subdir + os.sep + "male" + os.sep + file)
+                    males.add(name)
+                elif (numShes + numHer) > (numHes + numHis):
+                    os.rename(filepath, subdir + os.sep + "female" + os.sep + file)
+                    females.add(name) 
+                else:
+                    unsure.add(name)
 
-print("Males: %s" % males)
-print("females: %s" % females)
-print("unsure: %s" % unsure)
+print("Males: %s" % len(males))
+print("females: %s" % len(females))
+print("unsure: %s" % len(unsure))
 
