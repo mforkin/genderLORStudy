130e38c12e12d220e0abcfcdccc8810f8fcfd5d1
Mike Forkin <mforkin@ccri.com>
2018-05-28 21:52:07 -0400

    More progress

--- scripts/wordCounts.py
+++ scripts/wordCounts.py
@@ -9,13 +9,23 @@ outfile = '/Users/mike/resumeData/out/'
 
 males = 0
 females = 0
+female_grams = {}
+male_grams = {}
 
 for subdir, dirs, files in os.walk(rootdir):
     for file in files:
+        filepath = subdir + os.sep + file
+        is_female = False
         if subdir.endswith('female'):
             females += 1 
+            is_female = True 
         elif subdir.endswith('male'):
             males += 1
+        with open(filepath, 'r') as f:
+            content = f.read().replace('.', '').replace('!', '').replace('?', '')
+            grams = skipgrams(content)
+            for g in grams:
+                # add to appropriate map
 
 print("Females: %s" % females)
 print("Males: %s" % males)
